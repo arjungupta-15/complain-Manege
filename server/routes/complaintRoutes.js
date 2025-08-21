54ccec340223621fb5762a28255f93e54ca1dac4
arjungupta-15 <ag.arjungupta15@gmail.com>
2025-08-21 11:40:47 +0530

    commit

--- server/routes/complaintRoutes.js
+++ server/routes/complaintRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const complaintController = require('../controllers/complaintController');
 
 router.post('/submit_complaint', complaintController.submitComplaint);
+// Specific routes first (more specific than :id)
+router.get('/complaint/by-tracking/:trackingId', complaintController.getComplaintByTrackingId);
+router.get('/complaints-by-email', complaintController.getComplaintsByEmail);
+// Generic routes last
 router.get('/complaints/:id', complaintController.getComplaintById);
 router.get('/complaints', complaintController.getAllComplaints);
 router.put('/complaints/:id/status', complaintController.updateComplaintStatus);
-// New routes for student dashboard
-router.get('/complaints-by-email', complaintController.getComplaintsByEmail);
-router.get('/complaint/by-tracking/:trackingId', complaintController.getComplaintByTrackingId);
 
 module.exports = router;
